2c8ac32ed2c56d2cb18a995ef338311a2014eb8e
kitaminka <kit4minka@gmail.com>
2026-01-08 20:37:22 +0200

    update tui

--- note_editor/note_manager.py
+++ note_editor/note_manager.py
@@ -1,8 +1,16 @@
 import os
 
 class NoteManager:
-    def __init__(self, path):
-        self.path = path
+    def __init__(self, notes_dir):
+        self.notes_dir = notes_dir
 
     def list_notes(self) -> list[str]:
-        return [file.removesuffix(".md") for file in os.listdir(self.path) if file.endswith(".md")]
+        return [file.removesuffix(".md") for file in os.listdir(self.notes_dir) if file.endswith(".md")]
+    
+    def read_note(self, name) -> str:
+        with open(f"{self.notes_dir}/{name}.md", encoding="utf-8") as f:
+            return f.read()
+    
+    def write_note(self, name, content) -> None:
+        with open(f"{self.notes_dir}/{name}.md", "w") as f:
+            f.write(content)
--- note_editor/tui.py
+++ note_editor/tui.py
@@ -1,6 +1,6 @@
 from textual.app import App, ComposeResult
-from textual.widgets import Header, Footer, Markdown, TextArea, ListView, ListItem, Label, Placeholder
-from textual.containers import Horizontal, Vertical
+from textual.widgets import Header, Footer, Markdown, TextArea, ListView, ListItem, Label
+from textual.containers import Horizontal, VerticalScroll
 
 from note_manager import NoteManager
 
@@ -15,20 +15,22 @@ class NoteEditorApp(App):
     def compose(self) -> ComposeResult:
         note_items = [ListItem(Label(name)) for name in self.notes.list_notes()]
         self.editor = TextArea.code_editor("", language="markdown")
+        self.viewer = Markdown("", id="markdown_viewer")
         self.list_view = ListView(*note_items, id="notes")
 
         yield Header()
         yield Horizontal(
             self.list_view,
-            self.editor,
+            self.editor
+            # VerticalScroll(self.viewer, id="viewer_container", can_focus=True)
         )
         yield Footer()
     
+    def on_list_view_selected(self, event: ListView.Selected) -> None:
+        self.editor.focus()
+
     def on_list_view_highlighted(self, event: ListView.Highlighted) -> None:
         item = event.item
         label = item.query_one(Label)
-        self.editor.text = f"{label.content} loaded!"
-
-if __name__ == "__main__":
-    app = NoteEditorApp()
-    app.run()
+        # self.viewer.update(self.notes.read_note(label.content))
+        self.editor.load_text(self.notes.read_note(label.content))
